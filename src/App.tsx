4a2fa9545049ec9e9a9a8a79929c4853eea54e9f
haixiangyan <haixiang6123@gmail.com>
2022-12-09 01:05:05 +0800

    feat: 实现视频流的交互

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
-import React, {useRef, useState} from 'react';
+import React, {useCallback, useEffect, useRef, useState} from 'react';
 import NavBar from "./components/NavBar";
 import Tabs from "./components/Tabs";
 import BannerImage from './assets/banner.png';
 import FooterImage from './assets/footer.jpg';
 import Category from "./components/Cateogry";
+import { debounce } from 'lodash';
 
 import styles from './styles.module.scss';
 import {dataSource} from "./constants/data";
@@ -20,14 +21,93 @@ import classNames from "classnames";
 //   3.4 初始时，播放头两个视频
 //   3.5 横向滚动时，在可视窗口时播放
 
+const isInView = (el: HTMLVideoElement) => {
+  const { top, bottom, left, right } = el.getBoundingClientRect();
+
+  // 水平方向
+  const isHorizontalInView = 0 < left && right < window.innerWidth;
+  // 垂直方向
+  const isVerticalInView = top < window.innerHeight / 2 && window.innerHeight / 2 < bottom;
+  // 最终结果
+  return isHorizontalInView && isVerticalInView;
+}
+
 const App = () => {
   const oldYRef = useRef<number>(0);
   const contentRef = useRef<HTMLDivElement>(null);
   const offsetRef = useRef<HTMLDivElement>(null);
+  const playingIds = useRef<string[]>([])
+  const isScrolling = useRef<boolean>(false);
 
   const [hidden, setHidden] = useState<boolean>(false);
 
+  const playAll = (ids: string[]) => {
+    if (ids.length === 0) {
+      return;
+    }
+
+    const selector = ids.map(id => `[data-video-id="${id}"]`).join(',');
+    const videoEls: HTMLVideoElement[] = Array.from(document.querySelectorAll(selector));
+
+    videoEls.forEach(el => el.play())
+
+    playingIds.current = ids;
+  }
+
+  const stopAll = (ids: string[]) => {
+    if (ids.length === 0) {
+      return;
+    }
+
+    const selector = ids.map(id => `[data-video-id="${id}"]`).join(',');
+    const videoEls: HTMLVideoElement[] = Array.from(document.querySelectorAll(selector));
+
+    videoEls.forEach(el => {
+      el.pause();
+      el.currentTime = 0;
+    })
+  }
+
+  const pauseAll = (ids: string[]) => {
+    if (ids.length === 0) {
+      return;
+    }
+
+    const selector = ids.map(id => `[data-video-id="${id}"]`).join(',');
+    const videoEls: HTMLVideoElement[] = Array.from(document.querySelectorAll(selector));
+
+    videoEls.forEach(el => el.pause())
+  }
+
+  const onScrollEnd = useCallback(debounce(() => {
+    const videoEls = Array.from(document.querySelectorAll('video'));
+
+    // 找到命中规则的视频
+    const inViewVideoEls = videoEls.filter(el => isInView(el));
+
+    if (inViewVideoEls.length > 0) {
+      const ids: string[] = inViewVideoEls.map(el => el.getAttribute('data-video-id') || '');
+
+      // 旧视频（以前的 Id 不在这次播放列表中的）
+      const stopIds = playingIds.current.filter(id => !ids.includes(id));
+      stopAll(stopIds);
+
+      // 播放新视频
+      playAll(ids);
+    } else {
+      playAll(playingIds.current);
+    }
+
+    isScrolling.current = false;
+  }, 500), [])
+
   const onScroll = () => {
+    if (!isScrolling.current) {
+      pauseAll(playingIds.current);
+    }
+
+    isScrolling.current = true;
+
     if (contentRef.current && offsetRef.current) {
       const { bottom: offsetBottom } = offsetRef.current.getBoundingClientRect();
 
@@ -44,7 +124,15 @@ const App = () => {
         setHidden(delta < 0);
       }
     }
+
+    // 停下来超过 500ms 则认为是 scroll end
+    onScrollEnd();
   };
+
+  useEffect(() => {
+    const initVideoIds = dataSource.hot.list.slice(0, 2).map(item => item.id);
+    playAll(initVideoIds);
+  }, []);
 
   return (
     <div className={styles.app}>
@@ -63,13 +151,13 @@ const App = () => {
 
         <div ref={contentRef} className={styles.content}>
           <h2>{dataSource.hot.title}</h2>
-          <Category list={dataSource.hot.list} />
+          <Category onScroll={onScroll} list={dataSource.hot.list} />
 
           <h2>{dataSource.live.title}</h2>
-          <Category list={dataSource.live.list} />
+          <Category onScroll={onScroll} list={dataSource.live.list} />
 
           <h2>{dataSource.recommend.title}</h2>
-          <Category list={dataSource.recommend.list} />
+          <Category onScroll={onScroll} list={dataSource.recommend.list} />
         </div>
 
         <img className={styles.banner} src={FooterImage} alt="Banner"/>
--- src/components/Cateogry/index.tsx
+++ src/components/Cateogry/index.tsx
@@ -1,20 +1,20 @@
-import {FC} from "react";
+import {FC, HTMLAttributes} from "react";
 import {VideoData} from "../../constants/data";
 import styles from './styles.module.scss';
 
-interface Props {
+interface Props extends HTMLAttributes<HTMLDivElement> {
   list: VideoData[];
 }
 
 const Category: FC<Props> = (props) => {
-  const { list } = props;
+  const { list, ...divProps } = props;
 
   return (
-    <div className={styles.category}>
+    <div {...divProps} className={styles.category}>
       <ul>
         {list.map(videoData => (
           <li key={videoData.id}>
-            <video src={videoData.src}></video>
+            <video data-video-id={videoData.id} loop muted src={videoData.src}></video>
           </li>
         ))}
       </ul>
